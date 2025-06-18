a2b18a3e18875ca9ec84763ea0cf48a92203cef4
Daniel Beltran <danielesbe9526@hotmail.com>
2025-06-18 09:36:38 -0500

    Initial Commit

---
+++ MercadoLibreMobileChallenge/MercadoLibreMobileChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MercadoLibreMobileChallengeApp.swift
+//  MercadoLibreMobileChallenge
+//
+//  Created by Daniel Beltran on 18/06/25.
+//
+
+import SwiftUI
+
+@main
+struct MercadoLibreMobileChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
